c8695b71695dcbf77798b982ba03d57979bc686d
ahmed nasser <ahmednas197@gmail.com>
2023-01-11 00:54:54 +0200

    in memory datastore

---
+++ server/datastore/CommentDao.ts
@@ -0,0 +1,6 @@
+import { Comment } from "../types";
+export interface CommentDao {
+  createComment(comment: Comment): void;
+  listComment(postId: string): Comment[];
+  deleteComment(id: string): void;
+}
---
+++ server/datastore/index.ts
@@ -0,0 +1,9 @@
+import { CommentDao } from "./CommentDao";
+import { LikeDao } from "./LikeDao";
+import { PostDao } from "./PostDao";
+import { UserDao } from "./UserDao";
+import { InMemoryDataStore } from "./memorydb/index";
+
+export interface DataStore extends UserDao, PostDao, LikeDao, CommentDao {}
+
+export const db = new InMemoryDataStore();
